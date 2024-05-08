df550e4a4d29649e034b39273b75a8b0e0449171
00014227 <ahad60638@gmail.com>
2024-05-08 17:15:23 +0500

    fifth

--- app/components/Burger.js
+++ app/components/Burger.js
@@ -2,31 +2,21 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from "react-scroll"
 
-export default function Burger({close }) {
-    console.log(close, 'aaaaaaaaaa')
-    const [open, setOpen] = useState(close)
+export default function Burger({close, handleClick }) {
 
  
-    useEffect(() => {
-        setOpen(close);
-    }, [close]);
-
-    const handleMenuToggle = () => {
-        setOpen(!open);
-       
-    };
 
     return (
         <div className="lg:hidden w-full bg-[#404040] h-[4rem] flex items-center justify-end pr-4">
-            <button onClick={handleMenuToggle} className=' '>
+            <button onClick={handleClick} className=' '>
                 <svg width="50px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 18L20 18" stroke="#F39F1F" stroke-width="2" stroke-linecap="round" />
-                    <path d="M4 12L20 12" stroke="#F39F1F" stroke-width="2" stroke-linecap="round" />
-                    <path d="M4 6L20 6" stroke="#F39F1F" stroke-width="2" stroke-linecap="round" />
+                    <path d="M4 18L20 18" stroke="#F39F1F" strokeWidth="2" strokeLinecap="round" />
+                    <path d="M4 12L20 12" stroke="#F39F1F" strokeWidth="2" strokeLinecap="round" />
+                    <path d="M4 6L20 6" stroke="#F39F1F" strokeWidth="2" strokeLinecap="round" />
                 </svg>
             </button>
 
-            {open ? (
+            {close ? (
                 <div className="bg-[#404040] p-[5rem] w-[25rem] opacity-90 absolute top-[4.5rem] transition-opacity duration-300 ease-in-out">
                     <ul className=" space-y-4">
                         <li className="text-[#F39F1F] 2xl:text-2xl lg:text-xl font-semibold uppercase"><Link to="hero"
--- app/page.js
+++ app/page.js
@@ -58,23 +58,28 @@ export default function Home() {
 
   };
 
+  const handleClose = () => {
+    setClose(false)
+  }
+  
   const handleBurgerMenuOpen = () => {
-    console.log('Closing burger menu');
 
-    setClose(close => !close); // Close the burger menu when it's opened in the child component
+   
+    setClose(!close); // Close the burger menu when it's opened in the child component
+
 };
 
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between px-24 bg-[#404040]">
     <main  className="flex min-h-screen flex-col bg-[#404040]">
-      <section onClick={handleBurgerMenuOpen} className=" lg:h-screen" id="hero">
-        <Burger close = {close}  />
+      <section  className=" lg:h-screen" id="hero">
+        <Burger close = {close} handleClick={handleBurgerMenuOpen} />
         <div className="lg:hidden w-screen h-[1rem] bg-[#F39F1F]"></div>
         <div className=" lg:flex items-center 2xl:py-8 py-5 lg:px-24 px-20 justify-between  lg:border-b-0 border-b border-white space-y-4">
           <h1 className=" text-[#F39F1F] font-extrabold 2xl:text-5xl lg:text-4xl text-3xl uppercase">эвакуатор 725</h1>
           <div className="flex items-center lg:gap-4 gap-2 ml-4 ">
 
-            <Image src={Time} className=" lg:w-[2.5rem] w-[1.8rem] h-[1.9rem] lg:h-[2.6rem]" />
+            <Image src={Time} alt='time' className=" lg:w-[2.5rem] w-[1.8rem] h-[1.9rem] lg:h-[2.6rem]" />
 
             <p className=" lg:block hidden text-white 2xl:text-4xl lg:text-2xl">Круглосуточно <br /> без выходных</p>
             <p className="lg:hidden block text-white 2xl:text-4xl lg:text-2xl">Круглосуточно без выходных</p>
@@ -82,16 +87,16 @@ export default function Home() {
           </div>
 
           <div className="flex lg:gap-4 gap-2 items-center ml-10">
-            <Image src={Phone} className=" lg:w-[2.5rem] w-[1.8rem] h-[1.9rem] lg:h-[2.6rem]" />
+            <Image src={Phone} alt='phone' className=" lg:w-[2.5rem] w-[1.8rem] h-[1.9rem] lg:h-[2.6rem]" />
             <p className="text-[#F39F1F] font-extrabold 2xl:text-5xl lg:text-4xl uppercase">+998  (95 ) 055-55-55</p>
           </div>
         </div>
 
         <Navbar />
 
-        <dvi  className="lg:flex justify-center items-center 2xl:gap-[9rem] lg:gap-[4rem]">
+        <dvi  onClick={handleClose} className="lg:flex justify-center items-center 2xl:gap-[9rem] lg:gap-[4rem]">
           <div className=" 2xl:mt-[6rem] lg:px-0 px-10">
-            <Image src={Track} />
+            <Image src={Track} alt='track' />
           </div>
 
           <form onSubmit={handleSubmit} className=" flex flex-col lg:items-end items-center space-y-6 lg:mb-[10rem] mb-4 lg:px-0 px-10">
@@ -165,64 +170,64 @@ export default function Home() {
         <div className="lg:flex md:grid grid-cols-2 gap-5 mx-auto justify-between mt-12 lg:space-y-0 space-y-4">
 
 
-          <div class=" max-w-[300px] bg-white flex flex-col justify-center items-center border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className=" max-w-[300px] bg-white flex flex-col justify-center items-center border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-            <Image class="rounded-t-lg mt-10" src={Bike} alt="" />
+            <Image className="rounded-t-lg mt-10" src={Bike} alt="bike" />
 
-            <div class="p-5 space-y-7">
+            <div className="p-5 space-y-7">
 
-              <h5 class="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Эвакуация мотоциклов</h5>
+              <h5 className="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Эвакуация мотоциклов</h5>
 
               <p className=" text-xl text-center">от <span className=" font-bold text-2xl">100.000</span> сум</p>
-              <a href="tel:+998950555555" class="inline-flex items-center ml-[4rem]  px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <a href="tel:+998950555555" className="inline-flex items-center ml-[4rem]  px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Заказать
 
               </a>
             </div>
           </div>
 
-          <div class=" max-w-[300px] flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className=" max-w-[300px] flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-            <Image class="rounded-t-lg mt-10" src={Car} />
+            <Image className="rounded-t-lg mt-10" src={Car} alt='car'/>
 
-            <div class="p-5 space-y-7">
+            <div className="p-5 space-y-7">
 
-              <h5 class="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Эвакуация легковых автомобилей</h5>
+              <h5 className="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Эвакуация легковых автомобилей</h5>
 
               <p className=" text-xl text-center">от <span className=" font-bold text-2xl">200.000</span> сум</p>
-              <a href="tel:+998950555555" class="inline-flex items-center ml-[5rem]    px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <a href="tel:+998950555555" className="inline-flex items-center ml-[5rem]    px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Заказать
 
               </a>
             </div>
           </div>
 
-          <div class=" max-w-[300px] flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className=" max-w-[300px] flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-            <Image class="rounded-t-lg mt-10" src={Jeep} />
+            <Image className="rounded-t-lg mt-10" src={Jeep} alt = 'jeep'/>
 
-            <div class="p-5 space-y-7">
+            <div className="p-5 space-y-7">
 
-              <h5 class="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Эвакуация внедорожников до 4т</h5>
+              <h5 className="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Эвакуация внедорожников до 4т</h5>
 
               <p className=" text-xl text-center">от <span className=" font-bold text-2xl">300.000</span> сум</p>
-              <a href="tel:+998950555555" class="inline-flex items-center ml-[5rem]    px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <a href="tel:+998950555555" className="inline-flex items-center ml-[5rem]    px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Заказать
 
               </a>
             </div>
           </div>
 
-          <div class=" max-w-[300px] flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className=" max-w-[300px] flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-            <Image class="rounded-t-lg mt-10" src={SpecTeck} />
+            <Image className="rounded-t-lg mt-10" src={SpecTeck} alt='special track'/>
 
-            <div class="p-5 space-y-7">
+            <div className="p-5 space-y-7">
 
-              <h5 class="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Перевозка спецтехники до 4т</h5>
+              <h5 className="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Перевозка спецтехники до 4т</h5>
 
               <p className=" text-xl text-center">от <span className=" font-bold text-2xl">400.000</span> сум</p>
-              <a href="tel:+998950555555" class="inline-flex items-center ml-[5rem]    px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <a href="tel:+998950555555" className="inline-flex items-center ml-[5rem]    px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Заказать
 
               </a>
@@ -248,25 +253,25 @@ export default function Home() {
       <section className=" py-20 px-16">
         <div className="lg:flex md:grid grid-cols-2 justify-between items-center ">
           <div className="flex flex-col justify-center items-center">
-            <Image src={Pr1} />
+            <Image src={Pr1} alt="money"/>
             <p className=" mt-4 uppercase text-white text-xl">недорого</p>
             <p className=" text-white w-[15rem] text-center">Гарантия низкой цены. Зачем переплачивать когда есть мы? Эвакуация авто от 100.000 сум;</p>
           </div>
 
           <div className="flex flex-col justify-center items-center lg:mt-0 mt-[3rem]">
-            <Image src={Pr2} />
+            <Image src={Pr2} alt='save time'/>
             <p className="mt-4 uppercase text-white text-xl">СРОЧНО 24/7</p>
             <p className=" text-white w-[15rem] text-center">Средняя подача эвакуатора 20-25 минут. Работаем во всех районах города Ташкент;</p>
           </div>
 
           <div className="flex flex-col justify-center items-center mt-[3rem]">
-            <Image src={Pr3} />
+            <Image src={Pr3} alt='save'/>
             <p className="mt-4 uppercase text-white text-xl">С НАМИ БЕЗОПАСНО</p>
             <p className=" text-white w-[15rem] text-center">Аккуратно погрузим и бережно транспортируем в любую точку города Ташкент.  Принимаем к оплате банковские переводы;</p>
           </div>
 
           <div className="flex flex-col justify-center items-center lg:mt-[1.5rem] mt-[3rem]">
-            <Image src={Pr4} />
+            <Image src={Pr4} alt='list'/>
             <p className="mt-4 uppercase text-white text-xl">ВСЕ ВИДЫ УСЛУГ</p>
             <p className=" text-white w-[15rem] text-center">Гарантия низкой цены. Зачем переплачивать когда есть мы? Эвакуация авто от 100.000 сум;</p>
           </div>
@@ -279,17 +284,17 @@ export default function Home() {
           <p className=" text-[#F39F1F] mt-6 lg:text-3xl text-center text-2xl font-bold">Эвакуатор 725 дешево у нас чем у других компаний и «частников»</p>
 
           <div className="flex lg:gap-[3rem] gap-[1.5rem]  items-center justify-center">
-            <Image src={Galochka} className=' w-[3.5rem] h-[3.5rem]' />
+            <Image src={Galochka} alt='galochka' className=' w-[3.5rem] h-[3.5rem]' />
             <p className="lg:w-[70rem] text-[#404040] mt-16 font-medium lg:text-2xl text-xl"><b>Быстрое реагирование и оперативность:</b> Наша компания гарантирует оперативное реагирование на вызовы клиентов. Мы понимаем, что каждая минута важна, особенно в случае аварийных ситуаций на дороге. Наша служба эвакуации прибудет к вам в кратчайшие сроки, чтобы предоставить профессиональную помощь.</p>
           </div>
 
           <div className="flex lg:gap-[3rem] gap-[1.5rem]  items-center justify-center">
-            <Image src={Galochka} className=' w-[3.5rem] h-[3.5rem]' />
+            <Image src={Galochka} alt='galochka' className=' w-[3.5rem] h-[3.5rem]' />
             <p className="lg:w-[70rem] text-[#404040] mt-16 font-medium lg:text-2xl text-xl"><b>Опытные специалисты и современное оборудование:</b> Наша команда состоит из высококвалифицированных специалистов, которые обладают богатым опытом работы в области эвакуации транспорта. Мы также оснащены современным оборудованием, что позволяет нам выполнять задачи эффективно и безопасно.</p>
           </div>
 
           <div className="flex lg:gap-[3rem] gap-[1.5rem] items-center justify-center">
-            <Image src={Galochka} className=' w-[3.5rem] h-[3.5rem]' />
+            <Image src={Galochka} alt='galochka' className=' w-[3.5rem] h-[3.5rem]' />
             <p className="lg:w-[70rem] text-[#404040] mt-16 font-medium lg:text-2xl text-xl"><b>Прозрачная ценовая политика и высокий уровень обслуживания:</b> Мы ценим доверие наших клиентов, поэтому предлагаем прозрачную ценовую политику без скрытых дополнительных платежей. Наши клиенты всегда могут рассчитывать на профессиональное обслуживание и индивидуальный подход к каждой ситуации.</p>
           </div>
         </div>
@@ -300,31 +305,35 @@ export default function Home() {
       <footer className=" py-10 lg:px-20 px-10">
         <div className="lg:flex justify-between space-y-4">
           <div className="flex gap-5 items-center">
-            <Image className=" w-[2.5rem] h-[3rem]" src={Coins} />
+            <Image className=" w-[2.5rem] h-[3rem]" src={Coins} alt='coins' />
             <p className=" text-white 2xl:text-2xl lg:text-lg font-semibold uppercase">низкие<br /> цены</p>
           </div>
 
           <div className="flex gap-5 items-center">
-            <Image className=" w-[2.5rem] h-[3rem]" src={MiniTrack} />
+            <Image className=" w-[2.5rem] h-[3rem]" src={MiniTrack} alt='mini track'/>
             <p className=" text-white 2xl:text-2xl lg:text-lg font-semibold uppercase">быстрая<br />подача</p>
           </div>
 
           <div className="flex gap-5 items-center">
-            <Image className=" w-[2.5rem] h-[3rem]" src={AllDay} />
+            <Image className=" w-[2.5rem] h-[3rem]" src={AllDay} alt='all day'/>
             <p className=" text-white 2xl:text-2xl lg:text-lg font-semibold uppercase">Круглосуточно<br />и без выходных</p>
           </div>
 
           <div className=" gap-4 lg:flex hidden">
-            <Image src={Phone} className=" w-[2.5rem] h-[2.6rem]" />
+            <Image src={Phone} className=" w-[2.5rem] h-[2.6rem]" alt='phone' />
             <p className="text-[#F39F1F]  font-extrabold 2xl:text-5xl lg:text-4xl text-2xl  uppercase">+998  (95 ) 055-55-55</p>
           </div>
 
         </div>
         <div className=" gap-4 lg:hidden flex items-center mt-4">
-          <Image src={Phone} className=" w-[2.5rem] h-[2.6rem]" />
+          <Image src={Phone} className=" w-[2.5rem] h-[2.6rem]" alt='phone'/>
           <p className="text-[#F39F1F]  font-extrabold 2xl:text-5xl lg:text-4xl text-2xl  uppercase">+998  (95 ) 055-55-55</p>
         </div>
       </footer>
     </main>
   );
 }
+
+
+
+
